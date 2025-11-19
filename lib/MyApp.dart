57a4a3e3c0ff1e9da5857a0c47d8b46311237bae
Mohamad El Boustani <mhamadbesteni@gmail.com>
2025-11-19 12:07:14 +0200

    Separating "MyApp" from the main

---
+++ lib/MyApp.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Daily Transaction Summary",
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
